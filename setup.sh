6c6ff0d3ffc8ac3da35690c7f1dedeca0fa6d4b9
ウエスリー <wesley@cipolla.evokateur.net>
2025-12-03 13:44:40 -0800

    migrate to uv

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "[Re]creating virtual environment and installing dependencies.."
-python -m venv .venv --clear
-source .venv/bin/activate
-pip install -r requirements.txt
+echo "Making sure you have the latest version of uv.."
+uv self update
+
+echo "Creating virtual environment and installing dependencies.."
+uv sync
 
 echo "Installing Jupyter kernel spec for $(pwd)/.venv.."
-python -m ipykernel install --user --env VIRTUAL_ENV "$(pwd)/.venv" --name=pydantic-essentials --display-name "Pydantic Essentials"
+uv run python -m ipykernel install --user --env VIRTUAL_ENV "$(pwd)/.venv" --name=pydantic-essentials --display-name "Pydantic Essentials"
 
-echo "Select kernel 'pydantic-essentials' when running :MoltenInit"
+echo "Use the 'pydantic-essentials' kernel in Jupyter notebooks."
+echo
+echo "To launch Jupyter Lab in the uv environment:"
+echo " uv run --with jupyter jupyter lab"
+echo
+echo "To launch nvim in the uv environment (for molten.nvim):"
+echo " uv run nvim"
